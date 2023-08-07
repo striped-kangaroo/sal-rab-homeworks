2293683c6ce44891c4bc80dc8844d4231058f656
Maria Antonova <mail.mariant@gmail.com>
2023-08-07 15:53:39 +0700

    final task fixed

--- task_final/solution.js
+++ task_final/solution.js
@@ -47,7 +47,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = `${name} ${phone}`;
  
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
